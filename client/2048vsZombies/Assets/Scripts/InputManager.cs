d91ca2b9e165e38bc65e5d5c22938a4194331b53
SleepyHippo <zkchippo@gmail.com>
2016-06-26 08:29:29 +0800

    drag input

--- client/2048vsZombies/Assets/Scripts/InputManager.cs
+++ client/2048vsZombies/Assets/Scripts/InputManager.cs
@@ -4,23 +4,64 @@ using System.Collections;
 public class InputManager : MonoBehaviour {
 
     bool active;
+
+    bool startSwipe = false;
+    Vector3 startSwipPosition;
 	// Use this for initialization
 	void Start () {
         active = false;
         Messenger.AddListener(MessageConst.GAME_START, OnGameStart);
         Messenger.AddListener(MessageConst.GAME_OVER_START, OnGameOver);
 	}
-	
+
     void OnDestroy()
     {
         Messenger.RemoveListener(MessageConst.GAME_START, OnGameStart);
         Messenger.RemoveListener(MessageConst.GAME_OVER_START, OnGameOver);
     }
 
+//    void EasyTouch_On_Swipe (Gesture gesture)
+//    {
+//        if(gesture.swipe == EasyTouch.SwipeDirection.Left)
+//        {
+//            Messenger.Broadcast(MessageConst.INPUT_LEFT);
+//        }
+//        if(gesture.swipe == EasyTouch.SwipeDirection.Right)
+//        {
+//            Messenger.Broadcast(MessageConst.INPUT_RIGHT);
+//        }
+//        if(gesture.swipe == EasyTouch.SwipeDirection.Up)
+//        {
+//            Messenger.Broadcast(MessageConst.INPUT_UP);
+//        }
+//        if(gesture.swipe == EasyTouch.SwipeDirection.Down)
+//        {
+//            Messenger.Broadcast(MessageConst.INPUT_DOWN);
+//        }
+//    }
+
 	// Update is called once per frame
 	void Update () {
         if(active)
         {
+            if(Input.GetMouseButtonDown(0))
+            {
+                startSwipe = true;
+                startSwipPosition = Input.mousePosition;
+            }
+            if(Input.GetMouseButtonUp(0))
+            {
+                startSwipe = false;
+                Vector3 delta = Input.mousePosition - startSwipPosition;
+                if(delta.x > 0 && delta.x > Mathf.Abs(delta.y))
+                    Messenger.Broadcast(MessageConst.INPUT_RIGHT);
+                else if(delta.x < 0 && Mathf.Abs(delta.x) > delta.y)
+                    Messenger.Broadcast(MessageConst.INPUT_LEFT);
+                else if(delta.y > 0 && delta.y > Mathf.Abs(delta.x))
+                    Messenger.Broadcast(MessageConst.INPUT_UP);
+                else if(delta.y < 0 && Mathf.Abs(delta.y) > delta.x)
+                    Messenger.Broadcast(MessageConst.INPUT_DOWN);
+            }
             if(Input.GetKeyUp(KeyCode.LeftArrow))
             {
                 Messenger.Broadcast(MessageConst.INPUT_LEFT);
